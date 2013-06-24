c19b67158da981cb622c4c30e07d32d8790e1398
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-24 12:25:47 +1200

    persistent hover (only unhover when there is a new hover)

--- correspondence.js
+++ correspondence.js
@@ -2,9 +2,20 @@
 $(document).ready(function(){
   $("span[data-corrid]").hover(
     function() {
-      $(this).find("span").addClass("hover");
+      setHovering($(this));
     }, 
     function() {
-      $(this).find("span").removeClass("hover");
+      // nothing to unhover
     });
 });
+
+var currentHoveringElement = null;
+
+function setHovering(element) {
+  if (currentHoveringElement != null) {
+    currentHoveringElement.find("span").removeClass("hover");
+    currentHoveringElement = null;
+  }
+  element.find("span").addClass("hover");
+  currentHoveringElement = element;
+}
