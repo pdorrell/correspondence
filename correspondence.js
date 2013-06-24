52204e8df772af90b5e8f57e0f44c1d3e483f017
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-24 12:32:15 +1200

    clear current hover if clicking outside of other objects

--- correspondence.js
+++ correspondence.js
@@ -7,15 +7,25 @@ $(document).ready(function(){
     function() {
       // nothing to unhover
     });
+  $("body").click(
+    function(event) {
+      if (this == event.target) { // click outside of any inner elements to clear any current highlight
+        clearCurrentHoveringElement();
+      }
+    });
 });
 
 var currentHoveringElement = null;
 
-function setHovering(element) {
+function clearCurrentHoveringElement() {
   if (currentHoveringElement != null) {
     currentHoveringElement.find("span").removeClass("hover");
     currentHoveringElement = null;
   }
+}  
+
+function setHovering(element) {
+  clearCurrentHoveringElement();
   element.find("span").addClass("hover");
   currentHoveringElement = element;
 }
