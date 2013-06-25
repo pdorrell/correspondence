4c61df75e3788bc15137079d41315414936ef699
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-25 20:00:37 +1200

    rename initializer var

--- correspondence-examples.js
+++ correspondence-examples.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  var initializer = new CORRESPONDENCE.StructureGroups();
-  initializer.initializeStructureGroups($(".structure-group"));
-  $(initializer).on("mouseEnterItem", 
-                    function(event, item) { initializer.setSelected(item); });
-  $(initializer).on("clickOutsideItems", 
-                    function(event) { initializer.clearCurrentSelection(); });
+  var structureGroups = new CORRESPONDENCE.StructureGroups();
+  structureGroups.initializeStructureGroups($(".structure-group"));
+  $(structureGroups).on("mouseEnterItem", 
+                        function(event, item) { structureGroups.setSelected(item); });
+  $(structureGroups).on("clickOutsideItems", 
+                        function(event) { structureGroups.clearCurrentSelection(); });
 });
