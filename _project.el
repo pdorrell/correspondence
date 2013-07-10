6d42c6881b0124576c9eb4742e4aff62f72e8c0e
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-07-11 11:25:43 +1200

    include .scss in search extensions

--- _project.el
+++ _project.el
@@ -2,7 +2,7 @@
 
 (load-this-project
  `(
-   (:search-extensions (".js" ".html" ".css"))
+   (:search-extensions (".js" ".html" ".css" ".scss"))
    (:main-html-file "index.html")
    (:run-project-command (open-file-in-web-browser (project-file :main-html-file)))
    (:sass-watch-src-output-argument ,(concat (project-base-directory)))
